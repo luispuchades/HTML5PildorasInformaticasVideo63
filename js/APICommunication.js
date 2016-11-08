b2195d787fc2407a3b76688b0ba8c6bed555c318
luispuchades <luispuchades@live.com>
2016-11-08 07:25:35 +0100

    Actualización CSS ARISTALIA

--- js/APICommunication.js
+++ js/APICommunication.js
@@ -21,18 +21,37 @@ botonLeer = document.getElementById("boton-leer");
 
 //2. Definición de Funciones
 
+
+
+function mostrar(e) {
+    "use strict";
+
+//Publicamos en zonaDatos el contenido del evento generado
+// por el objeto creado con el constructor y que se
+// ha abierto con open y le pedimos que lo haga en
+// formato texto con responseText
+    zonaDatos.innerHTML = e.target.responseText;
+}
+
 function leer() {
     "use strict";
 
     var url;
     var solicitud;
-// Si el fichero está alojado en la misma ubicación que el documento HTML, no es necesario espedificar la ruta.
-    url = "texto.txt";
+// Si el fichero está alojado en la misma ubicación que el documento HTML, no es necesario especificar la ruta.
+    url = "../texto.txt";
 
 // Creamos un objeto "solicitud" con el constructor "new" de XMLHttpRequest();
     solicitud = new XMLHttpRequest();
 
+//Ponemos el objeto a la escucha
+    solicitud.addEventListener("load", mostrar, false);
 
+//Abrimos la conexión con un método GET a la dirección url
+    solicitud.open("GET", url, true);
+
+//Especificamos la información que vamos a enviar que en este caso es ninguna (null)
+    solicitu.send(null);
 }
 
 
